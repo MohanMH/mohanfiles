8c305bdfc05f8a5a58c9077555204bb327c7edc7
MohanMH <43338578+MohanMH@users.noreply.github.com>
2019-01-31 14:11:19 +0530

    Create string k_times.c

---
+++ string k_times.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+char str[5];
+  int k,i;
+printf("enter a string");
+scanf("%s",&str);
+printf("enter a limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("%s",str);
+}
+}
